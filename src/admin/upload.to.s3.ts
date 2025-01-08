c7656854debee19c7cf6d9f97d2cbee45a02514f
Kaisar Ahmad Bhat <kaisarahmad@calibraint.com>
2025-01-08 18:46:41 +0530

    used the recomended javascript v3 for AWS SDK and used the recomended method for saving images paths in S3 path

--- src/admin/upload.to.s3.ts
+++ src/admin/upload.to.s3.ts
@@ -3,23 +3,34 @@ import {
   ForbiddenException,
   Injectable,
 } from '@nestjs/common';
-import * as AWS from 'aws-sdk';
-import { S3 } from 'aws-sdk';
+import {
+  S3Client,
+  PutObjectCommand,
+} from '@aws-sdk/client-s3';
 
 @Injectable()
 export class S3Service {
-  private s3: S3;
+  private s3Client: S3Client;
+  private AWS_S3_BUCKET: string;
+
   constructor(private readonly config: ConfigService) {
-    this.s3 = new AWS.S3({
-      accessKeyId: this.config.get('S3_ACCESS_KEY_ID'),
-      secretAccessKey: this.config.get(
-        'S3_SECRET_ACCESS_KEY',
-      ),
+    const region = this.config.get('AWS_REGION');
+
+    this.s3Client = new S3Client({
+      credentials: {
+        accessKeyId: this.config.get('S3_ACCESS_KEY_ID'),
+        secretAccessKey: this.config.get(
+          'S3_SECRET_ACCESS_KEY',
+        ),
+      },
+      region,
+      endpoint: `https://s3.${region}.amazonaws.com`,
+      forcePathStyle: false,
     });
+
+    this.AWS_S3_BUCKET = this.config.get('S3_BUCKET');
   }
 
-  //AWS Details and connecting to s3 bucket
-  AWS_S3_BUCKET = this.config.get('S3_BUCKET');
   async uploadFile(file: Express.Multer.File) {
     const { originalname } = file;
     const imagesTypes = [
@@ -27,18 +38,21 @@ export class S3Service {
       'image/jpg',
       'image/jpeg',
     ];
-    //handling only  image types for upload
+
+    //handling only image types for upload
     if (!imagesTypes.includes(file.mimetype)) {
       throw new ForbiddenException(
         'File should be an image',
       );
     }
+
     //handling size of images 10 Mb max
     if (file.size > 1800 * 1944) {
       throw new ForbiddenException(
         'Image size should be less than 5 Mb',
       );
     }
+
     return await this.upload_To_S3(
       file.buffer,
       this.AWS_S3_BUCKET,
@@ -47,8 +61,12 @@ export class S3Service {
     );
   }
 
-  //uploading data to aws-s3-bucket
-  async upload_To_S3(file, bucket, name, mimetype) {
+  async upload_To_S3(
+    file: Buffer,
+    bucket: string,
+    name: string,
+    mimetype: string,
+  ): Promise<string> {
     const params = {
       Bucket: bucket,
       Key: String(name),
@@ -58,11 +76,13 @@ export class S3Service {
         'CONTENT_DISPOSITION',
       ),
     };
+
     try {
-      const s3Response = await this.s3
-        .upload(params)
-        .promise();
-      return s3Response.Location;
+      const command = new PutObjectCommand(params);
+      await this.s3Client.send(command);
+
+      // Constructing the URL using the correct region
+      return `${encodeURIComponent(name)}`;
     } catch (error) {
       throw error;
     }
